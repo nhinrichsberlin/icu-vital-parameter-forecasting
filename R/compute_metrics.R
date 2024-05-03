9dd279141cfef5f68e892e12ebfa415288ce2840
nhinrichsberlin <nhf.hinrichs@gmail.com>
2024-05-03 09:36:48 +0200

    update compute_metrics.R

--- R/compute_metrics.R
+++ R/compute_metrics.R
@@ -6,6 +6,16 @@ config <- yaml::read_yaml('config/config.yaml')
 # check that all forecasts exist as expected
 .check_expected_vs_forecasted(datasets = c("test", "eicu"))
 
-# store error metrics
+# store error metrics without grouping by hours_since_admission
 .store_error_metrics(targets = config$targets,
-                     models = c(config$ts_models, "blend", "gru", "transformer"))
+                     models = c(config$ts_models, 
+                                "blend",
+                                config$ml_models),
+                     by_hours_since_admission = FALSE)
+
+# store error metrics with grouping by hours_since_admission
+.store_error_metrics(targets = config$targets,
+                     models = c(config$ts_models, 
+                                "blend",
+                                config$ml_models),
+                     by_hours_since_admission = TRUE)
